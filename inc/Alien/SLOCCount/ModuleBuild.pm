623c4a35e2ce7241757958c3435b27e62d979894
Joenio Costa <joenio@joenio.me>
2016-09-19 19:39:17 -0300

    Initial import

---
+++ inc/Alien/SLOCCount/ModuleBuild.pm
@@ -0,0 +1,41 @@
+package Alien::SLOCCount::ModuleBuild;
+ 
+use parent 'Alien::Base::ModuleBuild';
+use IPC::Open3;
+
+sub alien_name {
+  'sloccount';
+}
+ 
+sub alien_check_installed_version {
+  if (`which sloccount`) {
+    my $pid = open3(\*CHLD_IN, \*CHLD_OUT, \*CHLD_ERR, "sloccount", "--version");
+    my $version = <CHLD_ERR>;
+    return $version;
+  }
+  else {
+    return undef;
+  }
+}
+
+sub alien_bin_requires {
+  {
+    'Alien::Autotools' => 0,
+    'Alien::flex' => 0,
+  }
+}
+
+#sub alien_build_commands {
+#  [ 'sh bootstrap', '%c --prefix=%s', 'make' ];
+#}
+
+sub alien_repository {
+  {
+    protocol => 'http',
+    host     => 'www.dwheeler.com',
+    location => '/sloccount',
+    pattern  => qr/^sloccount[_-]([\d\.-]+)\.tar\.gz$/,
+  };
+}
+
+1;
